994148ec2259ec3f607dbfe8201d04612c6520c4
Viktoriia Pigan <piganv@gmail.com>
2026-05-22 18:12:02 -0600

    test

--- src/main/java/homework/Main.java
+++ src/main/java/homework/Main.java
@@ -1,4 +1,11 @@
 package homework;
 
-public class main {
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(WeekDays.getDay(2)); // Monday
+        System.out.println(WeekDays.getDay(7)); // Saturday
+        System.out.println(WeekDays.getDay(0)); // error message
+    }
 }
