25dcb57a255ce61cfee09b598805be44e4531b79
Viktoriia Pigan <piganv@gmail.com>
2026-05-22 18:00:14 -0600

    test

--- src/main/java/homework/WeekDays.java
+++ src/main/java/homework/WeekDays.java
@@ -2,21 +2,32 @@ package homework;
 
 import java.util.ArrayList;
 
-public String getDay(int dayNumber) {
-    ArrayList<String> daysList = new ArrayList<>();
-    daysList.add("Sunday");
-    daysList.add("Monday");
-    daysList.add("Tuesday");
-    daysList.add("Wednesday");
-    daysList.add("Thursday");
-    daysList.add("Friday");
-    daysList.add("Saturday");
-    if (dayNumber == null) throw new NullPointerException();
-    else if (dayNumber < 1) {
-        return "The number should be equal or larger than 1";
-    } else if (dayNumber > 7) {
-        return "The number should be equal or smaller than 7";
-    } else {
+public class WeekDays {
+
+    public static String getDay(Integer dayNumber) {
+
+        ArrayList<String> daysList = new ArrayList<>();
+        daysList.add("Sunday");
+        daysList.add("Monday");
+        daysList.add("Tuesday");
+        daysList.add("Wednesday");
+        daysList.add("Thursday");
+        daysList.add("Friday");
+        daysList.add("Saturday");
+        daysList.add("Saturday1");
+
+        if (dayNumber == null) {
+            throw new NullPointerException();
+        }
+
+        if (dayNumber < 1) {
+            return "The number should be equal or larger than 1";
+        }
+
+        if (dayNumber > 7) {
+            return "The number should be equal or smaller than 7";
+        }
+
         return daysList.get(dayNumber - 1);
     }
 }
